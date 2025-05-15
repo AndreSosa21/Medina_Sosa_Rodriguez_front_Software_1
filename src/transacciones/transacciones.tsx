9a07850fda32a4be88d78403222fa8dc45c1049d
AndreSosa21 <andreasosro@unisabana.edu.co>
2025-05-15 14:11:49 -0500

    si

--- src/transacciones/transacciones.tsx
+++ src/transacciones/transacciones.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './Transacciones.css';
+import './transacciones.css';
 import { useNavigate } from 'react-router-dom';
 import { API_URL } from '../api';
 
